e2444b4d4592e7b7de87b180f03ae8188fde9957
ybqjty <1443567072@qq.com>
2016-10-31 20:33:07 +0800

    update

--- src/ybq/Mdzz.java
+++ src/ybq/Mdzz.java
@@ -3,6 +3,9 @@ package ybq;
 public class Mdzz {
 	
 	public static void main(String[] args) {
+		System.out.println("mm");
+		System.out.println("dd");
+		System.out.println("zz");
 		System.out.println("zz");
 	}
 }
